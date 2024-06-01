be13b88628b0c17f7fa898aaeefbede776483e2b
Fiza Azhar <fizaazhar54@gmail.com>
2024-06-01 17:17:55 +0500

    University Management System

---
+++ uams/uams/BL/DegreeProgram.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uams.BL
+{
+    class DegreeProgram
+    {
+        public string degreeName;
+        public float degreeDuration;
+        public List<Subject> subjects;
+        public int seats;
+
+        public DegreeProgram(string degreeName, float degreeDuration, int seats)
+        {
+            this.degreeName = degreeName;
+            this.degreeDuration = degreeDuration;
+            this.seats = seats;
+            subjects = new List<Subject>();
+        }
+
+        internal Subject Subject
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public bool isSubjectExists(Subject sub)
+        {
+            foreach (Subject s in subjects)
+            {
+                if (s.code == sub.code)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool AddSubject(Subject s)
+        {
+            int creditHours = calculateCreditHours();
+            if(creditHours + s.creditHours <= 20)
+            {
+                subjects.Add(s);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int calculateCreditHours()
+        {
+            int count = 0;
+            for (int x = 0; x < subjects.Count; x++)
+            {
+                count = count + subjects[x].creditHours;
+            }
+            return count;
+        }
+
+    }
+}
---
+++ uams/uams/UI/DegreeProgramUI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uams.BL;
+using uams.DL;
+
+namespace uams.UI
+{
+    class DegreeProgramUI
+    {
+        public static DegreeProgram takeInputForDegree()
+        {
+            string degreeName;
+            float degreeDuration;
+            int seats;
+            Console.Write("Enter Degree Name: ");
+            degreeName = Console.ReadLine();
+            Console.Write("Enter Degree Duration: ");
+            degreeDuration = float.Parse(Console.ReadLine());
+            Console.Write("Enter Seats for Degree: ");
+            seats = int.Parse(Console.ReadLine());
+            string subPath = "subject.txt";
+            DegreeProgram degProg = new DegreeProgram(degreeName, degreeDuration, seats);
+            Console.Write("Enter How many Subjects to Enter: ");
+            int count = int.Parse(Console.ReadLine());
+            for (int x = 0; x < count; x++)
+            {
+                Subject s = SubjectUI.takeInputForSubject();
+                if (degProg.AddSubject(s))
+                {
+                    if (!(SubjectDL.subjectList.Contains(s)))
+                    {
+                        SubjectDL.addSubjectIntoList(s);
+                        SubjectDL.storeintoFile(subPath, s);
+                    }
+
+                    Console.WriteLine("Subject Added");
+                }
+                else
+                {
+                    Console.WriteLine("Subject Not Added");
+                    Console.WriteLine("20 credit hour limit exceeded");
+                    x--;
+                }
+            }
+            return degProg;
+
+        }
+
+        public static void viewDegreePrograms()
+        {
+            foreach (DegreeProgram dp in DegreeProgramDL.programList)
+            {
+                Console.WriteLine(dp.degreeName);
+            }
+        }
+    }
+}
